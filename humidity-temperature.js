474b3984fce72892acb9b3995cee9dd84d2b27c0
Gloird <nicolas86200@gmail.com>
2023-05-23 21:02:02 +0200

    add HumidityTemperature

---
+++ humidity-temperature.js
@@ -0,0 +1,27 @@
+import { LitElement } from "card-tools/src/lit-element";
+//https://github.com/thomasloven/lovelace-card-tools
+
+customElements.whenDefined('card-tools').then(() => {
+    var cardTools = customElements.get('card-tools');
+  
+    class HumidityTemperature extends cardTools.LitElement {
+      setConfig(config) {
+        this.name = config.name;
+      }
+  
+      render() {
+        return cardTools.LitHtml`
+          ${this.name}
+        `;
+      }
+    }
+  
+    customElements.define("humidity-temperature", MyPlugin);
+  }); // END OF .then(() => {
+  
+  setTimeout(() => {
+    if(customElements.get('card-tools')) return;
+    customElements.define('humidity-temperature', class extends HTMLElement{
+      setConfig() { throw new Error("Can't find card-tools. See https://github.com/thomasloven/lovelace-card-tools");}
+    });
+  }, 2000);
